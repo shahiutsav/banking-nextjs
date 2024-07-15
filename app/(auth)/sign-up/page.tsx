90358c242dee0ee4682fed622cd07e52aadf52db
Utsav Shahi <92775677+shahiutsav@users.noreply.github.com>
2024-07-15 16:58:32 +0545

    Implement a basic sign-in and sign-up form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,11 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
-const Signup = () => {
-  return <div>Signup</div>;
+const SignUp = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
+  );
 };
-
-export default Signup;
+export default SignUp;
---
+++ components/CustomInput.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  FormControl,
+  FormField,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { Control, FieldPath } from "react-hook-form";
+import { z } from "zod";
+import { authformSchema } from "@/lib/utils";
+
+interface CustomInputProps {
+  control: Control<z.infer<typeof authformSchema>>;
+  name: FieldPath<z.infer<typeof authformSchema>>;
+  label: string;
+  placeholder: string;
+}
+
+const CustomInput = ({
+  control,
+  name,
+  label,
+  placeholder,
+}: CustomInputProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class"
+                type={name === "password" ? "password" : "text"}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="form-message mt-2" />
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default CustomInput;
