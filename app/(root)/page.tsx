1264c8c95b4962ad59a6093da9198effbd308d2d
Utsav Shahi <92775677+shahiutsav@users.noreply.github.com>
2024-07-15 09:24:21 +0545

    Implement the right sidebar in homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Ingine" };
+  const loggedIn = {
+    firstName: "Jon",
+    lastName: "Smith",
+    email: "jon.smith@mail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -20,7 +25,13 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
     </section>
   );
 };
